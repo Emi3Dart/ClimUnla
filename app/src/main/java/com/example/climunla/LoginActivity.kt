ffe7662ea2220163010b306a5387336c5f26ba3e
EmilianoGinarte <eginartedelgado@gmail.com>
2024-09-06 13:56:05 -0300

    commit inicial

---
+++ app/src/main/java/com/example/climunla/LoginActivity.kt
@@ -0,0 +1,68 @@
+package com.example.climunla
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
+
+class LoginActivity : AppCompatActivity() {
+
+
+    lateinit var lottieAnimacion : LottieAnimationView
+    lateinit var etUsuario : EditText
+    lateinit var etPassword : EditText
+    lateinit var btnRegistrarse : Button
+    lateinit var btnIniciarSesion : Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        etUsuario = findViewById(R.id.et_nombre_usuario)
+        etPassword = findViewById(R.id.et_contrasenia)
+        btnRegistrarse = findViewById(R.id.btn_contrasenia)
+        btnIniciarSesion = findViewById(R.id.btn_login)
+        lottieAnimacion = findViewById(R.id.la_wave)
+
+        lottieAnimacion.setSpeed(0.3f)
+        lottieAnimacion.playAnimation() /// para que la animacion de fondo no vaya tan rapido
+
+        btnRegistrarse.setOnClickListener {
+            Toast.makeText(this, "Crear Usuario - TODO", Toast.LENGTH_SHORT).show()
+            /*
+             val intent = Intent(this, TermsAndConditionsActivity::class.java)
+            startActivity(intent)
+            */
+        }
+        btnIniciarSesion.setOnClickListener {
+            var usuario = etUsuario.text.toString()
+            if(usuario.isEmpty() || etPassword.text.toString().isEmpty()){
+                var mensaje = "Completar Datos"
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"Iniciar sesion - TODO", Toast.LENGTH_SHORT).show()
+                /*
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("NOMBRE", usuario)
+                startActivity(intent)
+                finish()
+                */
+            }
+
+        }
+    }
+}
