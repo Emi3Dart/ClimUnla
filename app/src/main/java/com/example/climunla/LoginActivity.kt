b8e2a2e824902058a90ddc19bf4b992c8eac757a
zeraus1313 <julisuarez1313@gmail.com>
2024-09-29 19:40:31 -0300

    recordar usuario

--- app/src/main/java/com/example/climunla/LoginActivity.kt
+++ app/src/main/java/com/example/climunla/LoginActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.airbnb.lottie.LottieAnimationView
 import android.content.Intent
+import android.widget.CheckBox
 import android.widget.TextView
 import com.example.climunla.data.UsuarioDataBase
 import com.example.climunla.data.dao.UsuarioDao
@@ -26,6 +27,7 @@ class LoginActivity : AppCompatActivity() {
     lateinit var tvRegistrar: TextView
     private lateinit var db: UsuarioDataBase // Cambiado a lateinit para inicializar en onCreate
     private lateinit var usuarioDao: UsuarioDao // Cambiado a lateinit
+    lateinit var cbRecordar: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,9 +49,20 @@ class LoginActivity : AppCompatActivity() {
         btnIniciarSesion = findViewById(R.id.btn_login)
         lottieAnimacion = findViewById(R.id.la_wave)
         tvRegistrar = findViewById(R.id.tv_registrar)
+        cbRecordar = findViewById(R.id.cb_recordar) // Nuevo: CheckBox
 
         lottieAnimacion.setSpeed(0.3f)
         lottieAnimacion.playAnimation()
+
+        // Recuperar SharedPreferences
+        val sharedPreferences = getSharedPreferences("acceso", MODE_PRIVATE)
+        val recordarUsuario = sharedPreferences.getBoolean("recordar_usuario", false)
+
+        if (recordarUsuario) {
+            etUsuario.setText(sharedPreferences.getString("username", ""))
+            etPassword.setText(sharedPreferences.getString("password", ""))
+            cbRecordar.isChecked = true
+        }
 
         tvRegistrar.setOnClickListener {
             // Iniciar la actividad de registro
@@ -65,6 +78,18 @@ class LoginActivity : AppCompatActivity() {
             if (usuario.isEmpty() || contrasenia.isEmpty()) {
                 Toast.makeText(this, "Completar Datos", Toast.LENGTH_SHORT).show()
             } else {
+                val editor = sharedPreferences.edit()
+                if (cbRecordar.isChecked) {
+                    editor.putString("username",usuario)
+                    editor.putString("password", contrasenia)
+                    editor.putBoolean("recordar_usuario", true)
+                } else {
+                    // Si no está marcado, limpiar los datos guardados
+                    editor.remove("username")
+                    editor.remove("password")
+                    editor.putBoolean("recordar_usuario", false)
+                }
+                editor.apply()
                 CoroutineScope(Dispatchers.IO).launch {
                     try {
                         // Verifica las credenciales en la base de datos
