2a99d40685776017064e756e18b91c2e167e12c1
EmilianoGinarte <eginartedelgado@gmail.com>
2024-09-29 21:17:24 -0300

    ToolBar (Emi):      Terminado el toolbar del MainActivity      Terminado el toolbar del DetalleDia. con el icono arrow_Back nativo de Android Studio. Con la funcionalidad de volver para atras implementada.

--- app/src/main/java/com/example/climunla/Detalle_dia.kt
+++ app/src/main/java/com/example/climunla/Detalle_dia.kt
@@ -1,18 +1,41 @@
 package com.example.climunla
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.climunla.R
 
 class Detalle_dia : AppCompatActivity() {
+
+    lateinit var toolbar:Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_dia)
 
 
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = resources.getString(R.string.titulo)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_return, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.item_volver){
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
